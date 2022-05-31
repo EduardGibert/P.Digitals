38a8baa9f414f5e967608421e1268d19ec97bb39
EduardGibert <100867309+EduardGibert@users.noreply.github.com>
2022-05-31 13:45:09 +0200

    Add files via upload

---
+++ P4_EG/P4_Part2_EG/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+
+/* delete a task when finish,
+this will never happen because this is infinity loop */
+
+
+void funcion1(void * parameter)
+{
+#define led 16
+pinMode(led, OUTPUT);
+
+for(;;)
+{
+digitalWrite(led, HIGH);
+Serial.println("ON");
+delay(1000);
+digitalWrite(led, LOW);
+Serial.println("OFF");
+delay(1000);
+
+}
+
+}
+
+void setup()
+{
+Serial.begin(112500);
+
+xTaskCreate(
+funcion1, /* Task function. */
+"FUNCION 1", /* name of task. */
+10000, /* Stack size of task */
+NULL, /* parameter of the task */
+1, /* priority of the task */
+NULL); /* Task handle to keep track of created task */
+}
+
+
+void loop()
+{}
